a98fa93130a40a50ca23ff375f653273bd3467dc
karianjahi <nkarianjahi@gmail.com>
2026-01-28 15:31:36 +0100

    Create a function that picks an item at random

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const options = ["Rock", "Paper", "Scissors"];
+
+const getRandomComputerResult = () => {
+     const min = 0;
+     const max = 2;
+     const random = Math.floor(Math.random() * (max - min + 1));
+     return options[random];
+};
+
+console.log(getRandomComputerResult());
