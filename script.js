26c9f450d6d0cf139cc2ddb8ca3a54f770c0f746
karianjahi <nkarianjahi@gmail.com>
2026-01-28 15:52:20 +0100

    Refactor the function that picks an item at random

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const getRandomComputerResult = () => {
      const min = 0;
      const max = 2;
      const random = Math.floor(Math.random() * (max - min + 1));
+    //  console.log(typeof array[random]);
      return options[random];
 };
 
-console.log(getRandomComputerResult());
